1dda442cb7c71c77e330fd1798f3c6894fa9c89f
Kishor Vairal <KV00523493@techmahindra.com>
2019-01-30 17:17:54 +0530

    second commit done by me

--- src/main/java/TestBase/Login.java
+++ src/main/java/TestBase/Login.java
@@ -2,6 +2,11 @@ package TestBase;
 
 public class Login {
 
+	public void loginSet()
+	{
+		System.out.println("Login Set Methos");
+	}
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Kishor");
---
+++ src/main/java/TestBase/SearchPage.java
@@ -0,0 +1,10 @@
+package TestBase;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+
+        System.out.println("Search Page ");
+	}
+
+}
